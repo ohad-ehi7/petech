1d6ecd7c41d555eb9cf879c5abee0afaf0553c6e
loftyyyy <lofty08.business@gmail.com>
2025-04-20 00:31:30 +0800

    WIP: Landing Page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
+});
+
+Route::get('test', function () {
+    return view('test');
 });
